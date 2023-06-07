11df0fdf41f7496d363451d6bc33b7a773f5c1a0
Orest278 <125860863+Orest278@users.noreply.github.com>
2023-06-07 11:43:27 +0300

    run

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,34 @@
-import { useEffect } from 'react';
-import s from '../css/styles..module.css'
+import { useEffect, useCallback } from 'react';
+import s from '../css/styles..module.css';
 
-export const Modal = ({modalImage, closeModal}) => {
-  const closeByEscape = (e) => {
-    if (e.code === 'Escape') {
-      closeModal();
-    }
-  };
-
-  const closeByBackdrop = (event) => {
+export const Modal = ({ modalImage, closeModal }) => {
+  const closeByBackdrop = useCallback((event) => {
     if (event.currentTarget === event.target) {
       closeModal();
     }
-  };
+  }, [closeModal]);
 
   useEffect(() => {
+    const closeByEscape = (e) => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+
     window.addEventListener('keydown', closeByEscape);
-    
+
     return () => {
-    window.removeEventListener('keydown', closeByEscape);
-    }
-  }, [closeByEscape])
+      window.removeEventListener('keydown', closeByEscape);
+    };
+  }, [closeByBackdrop, closeModal]);
 
-
-
-    return (
-      <div className={s.Overlay} onClick={closeByBackdrop}>
-        <div className={s.Modal}>
-          <img src={modalImage.src} alt={modalImage.alt} />
-        </div>
+  return (
+    <div className={s.Overlay} onClick={closeByBackdrop}>
+      <div className={s.Modal}>
+        <img src={modalImage.src} alt={modalImage.alt} />
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Modal;
